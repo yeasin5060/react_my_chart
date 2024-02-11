5bd712db2946a9ac1758cf6a8fb2cbafe9504d63
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-02-11 14:28:07 +0600

    update_validation_009

--- src/Pages/Home/SectionHome/HomeUserlist/HomeUserlist.jsx
+++ src/Pages/Home/SectionHome/HomeUserlist/HomeUserlist.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './HomeUserlist.css'
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { LiaSearchSolid } from "react-icons/lia";
 import { FaPlus } from "react-icons/fa6";
 import Groupusers from '../../../../Components/Groupusers/Groupusers';
-import profileone from '../../../../images/profileone.png';
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
 
 const HomeUserlist = () => {
+
+    const alldata = useSelector((state) => state.logindata.value)
+    const db = getDatabase();
+    let [userList , setUserList] = useState([])
+    
+    useEffect(()=>{
+        const userListRef = ref(db, 'usersdata');
+            onValue(userListRef, (snapshot) => {
+            const data = snapshot.val();
+               let array = []
+            snapshot.forEach((item)=>{
+                if(alldata.uid != item.key){
+                    array.push({...item.val(),id:item.key})
+                }
+            })
+            setUserList(array)
+        });
+
+    },[])
   return (
     <section id='user_list'>
         <div className='user_list_search_box_wrapper'>
@@ -24,15 +44,17 @@ const HomeUserlist = () => {
             <Groupusers grouphead="User List">
                 <div className='user_list_profile_wrapper_box'>
                     {
-                        [ 1 , 2 , ].map((item , index)=>(
-                            <div className='user_list_profile_wrapper'>
+                        userList && userList.length > 0
+                        ?
+                        userList.map((item , index)=>(
+                            <div  key= {index} className='user_list_profile_wrapper'>
                                 <div className='user_list_profile_box'>
                                     <div className='user_list_profile_image'>
-                                        <img src={profileone} alt="" />
+                                        <img src={item.profileImage} alt="not found" />
                                     </div>
                             
                                     <div className='user_list_profile_name'>
-                                        <h4 className='user_list_profile_user_name'>Raghav</h4>
+                                        <h4 className='user_list_profile_user_name'>{item.username}</h4>
                                         <h5 className='user_list_profile_online'>56pm</h5>
                                     </div>
                                 </div>
@@ -43,6 +65,8 @@ const HomeUserlist = () => {
                                 </div>
                             </div>
                         ))
+                        :
+                        <h1>data nai</h1>
                     }
                 </div>
             </Groupusers>
